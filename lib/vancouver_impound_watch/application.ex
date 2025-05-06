7cdbdd982c3107a39d3ec5eab6951eb086204f8d
Landon Ajimura <ajimuralk@gmail.com>
2025-05-06 10:29:02 -0700

    Start Scheduler and ETS under supe in Application

--- lib/vancouver_impound_watch/application.ex
+++ lib/vancouver_impound_watch/application.ex
@@ -8,14 +8,19 @@ defmodule VancouverImpoundWatch.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # {VancouverImpoundWatch.Scheduler, x}
-      # Starts a worker by calling: VancouverImpoundWatch.Worker.start_link(arg)
-      # {VancouverImpoundWatch.Worker, arg}
+      {VancouverImpoundWatch.RegisteredPetTable, []},
+      with_opts(VancouverImpoundWatch.Scheduler)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: VancouverImpoundWatch.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp with_opts(module) do
+    opts = Application.get_env(:vancouver_impound_watch, module, [])
+
+    {module, opts}
   end
 end
